b379788ab65e3a516ddb3de9648efc8cf44b7269
bugswriter <surajkushwah6969@gmail.com>
2019-02-19 00:04:39 +0530

    Re-structured whole project in package format

--- run.py
+++ run.py
@@ -1,51 +1,5 @@
-from flask import Flask , render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "04beccaf060f0a03ebfc301930fc1c30"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{ self.username }', '{ self.email }', '{ self.image_file }')"
-
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'{ form.username.data } has been successfully logged in', category='success')
-        return redirect(url_for('about'))
-    
-    return render_template('login.html', title='Login', form = form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account is been created for { form.username.data }', category='success')
-        return redirect(url_for('login'))
-
-    return render_template('register.html', title='Register', form = form )
-
+from UniQx import app
 
 if __name__=='__main__':
     app.run(debug=True)
+ 
