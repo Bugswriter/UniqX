8acff433e7ee7e252e0dfffd82b6d35bda46341e
bugswriter <surajkushwah6969@gmail.com>
2019-02-25 00:25:00 +0530

    Adding methods into post/routes.py

--- UniQx/post/routes.py
+++ UniQx/post/routes.py
@@ -6,14 +6,15 @@ from UniQx.post.forms import PostForm
 
 post = Blueprint('post', __name__)
 
-@post.route('/post/new')
+@post.route('/post/new', methods=['GET', 'POST'])
 @login_required
 def create_post():
     form = PostForm()
     if form.validate_on_submit():
         post = Post(title=form.title.data, image=form.image.data, content=form.content.data)
-	db.session.add(post)
-	db.session.commit()
-	flash("Post have been created")
-	return redirect(url_for('main.about'))
+        db.session.add(post)
+        db.session.commit()
+        flash("Post have been created")
+        return redirect(url_for('main.about'))
+    
     return render_template('create_post.html', title='Profile', form = form)
